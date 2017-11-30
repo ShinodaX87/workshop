d598c004d99ff930458cefb9e34a80e4697e6400
zenbox <michael@zenbox.de>
2017-11-30 10:33:19 +0100

    30.11.2017

---
+++ assets/js/pattern/es6-promise.js
@@ -0,0 +1,25 @@
+/* global console, window, document */
+/**
+ * Promises
+ *
+ * @package Advanced Javascript
+ * @author Michael [michael@zenbox.de]
+ * @since 2017/11/30
+ * @version v1.0.0
+ * @copyright (c) 2017 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+
+(function () {
+  'use strict';
+  // - - - - - - - - - -
+  // DECLARATION
+  let get = null;
+
+  // METHODS
+  get = function () {};
+
+  // CONTROL
+  get('story.json');
+  // - - - - - - - - - -
+}());
