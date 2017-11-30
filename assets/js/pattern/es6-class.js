3ff2716c259803f7f9f16117e40f61954061e2cf
zenbox <michael@zenbox.de>
2017-11-30 10:33:08 +0100

    30.11.2017

--- assets/js/pattern/es6-class.js
+++ assets/js/pattern/es6-class.js
@@ -39,8 +39,60 @@
     }
   }
 
-  let myShape = new Shape('1', 20, 20);
+  /**
+   * [Rectangle description]
+   * @extends Shape
+   */
+  class Rectangle extends Shape {
+    /**
+     * [constructor description]
+     * @param  {string} id     [description]
+     * @param  {number} x      [description]
+     * @param  {number} y      [description]
+     * @param  {number} width  [description]
+     * @param  {number} height [description]
+     */
+    constructor(id, x, y, width, height) {
+      super(id, x, y);
+      this.width = width;
+      this.height = height;
+    }
+  }
+  /**
+   * [Circle description]
+   * @extends Shape
+   */
+  class Circle extends Shape {
+    /**
+     * [constructor description]
+     * @param  {string} id     [description]
+     * @param  {number} x      [description]
+     * @param  {number} y      [description]
+     * @param  {number} radius [description]
+     */
+    constructor(id, x, y, radius) {
+      super(id, x, y);
+      this.radius = radius;
+    }
+    /**
+     * [test description]
+     * @return {[type]} [description]
+     */
+    test() {
+      let array = [1, 2, 3];
+      for (let i = 0; i < array.length; i++) {
+        array[i] = 1;
+      }
+    }
+  }
+
+  let myShape = new Shape('1', 20, 20),
+    myRectangle = new Rectangle('2', 10, 10, 20, 20),
+    myCircle = new Circle('3', 130, 30, 50);
+
   myShape.move(40, 40);
+  myRectangle.move(40, 40);
+  myCircle.move(40, 40);
 
   console.log(myShape);
   // - - - - - - - - - -
