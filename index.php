631005091de18761362bbb18f48eddbaa961713c
zenbox <michael@zenbox.de>
2018-02-05 13:12:51 +0100

    05.02.2018

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
 <html>
 <head>
+ <meta charset="utf-16">
   <title>ein erstes Dokument</title>
 </head>
 <body>
@@ -14,5 +16,42 @@
 <?php
 // phpinfo(); // Augabe aller Informatinonen zur PHP Installation
 $text = "<p>Hello World</p>";
+$my_text = "lalala";
+$myNewTextFromDatabase = "lalala"; // lower camel case
+$myText123 = "lalala";
+
 echo $text;
+
+// einzeilige Kommentarzeile
+# auch einzeilig, ist aber veraltet
+/*
+  mehrzeilige Kommentarzeile
+  da kann vieles in
+  vielen Zeilen stehen
+ */
+/**
+ * mehrzeiliger Kommentar nach JavaDoc/PHPDoc
+ * @author Michael
+ * @since  2018/02/05
+ * @version 0.0.1
+ * @package my first PHP app
+ */
+
+// einfache Variablentypen
+$a = 42;
+$string = "Beliebig, $a Zahlen 123 und anderes !!!"; // Literale bestehen aus beliebige Zeichen
+$string2 = 'auch möglich. $a'; // default für Strings!
+$integer = 123;
+$float = 3.14152;
+$boolean = false;
+
+
+echo $string;
+echo $string2;
+echo '<br>';
+echo $integer;
+echo '<br>';
+echo $float;
 ?>
+</body>
+</html>
