491c23d754c54ca6810c783d80050035428fc750
zenbox <michael@zenbox.de>
2018-02-06 12:00:20 +0100

    06.02.2018

---
+++ includes/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// Eigene Funktionen entwickeln
+function pruefe($value)
+{
+    // Führende und anhängede Leerzeichen und Tabs abschneiden
+  $value = trim($value);
+  // SQL Injections
+  // $value = mysql_escape_string($value);
+
+    if ($value !== '') {
+        return $value;
+    }
+    return false;
+}
+
+function andereFuntion()
+{
+}
--- login.php
+++ login.php
@@ -1,3 +1,23 @@
+<?php
+  /**
+   * Login Daten prüfen
+   * @author Michael
+   * @version 0.0.1
+   */
+  // Dateien einbinden
+  include('includes/functions.php');
+
+  // Daten aus dem CGI annehmen
+  // $_GET/$_POST sind Arrays
+  $email    = pruefe($_GET['email']);
+  $password = pruefe($_GET['password']);
+
+  // wenn gültige Eingaben geliefert sind
+  if ($email !== false && $password !== false) {
+      //$result = frageDatenbank($email);
+  }
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,24 +26,33 @@
   </head>
   <body>
 
+    <!-- Formular bei Erstaufruf -->
     <h1>Login</h1>
 
-    <form action="login.php" method="post">
+    <form action="login.php" method="get">
 
       <fieldset>
         <legend>Benutzerlogin</legend>
 
         <label for="login-email">Email-Adresse</label>
-        <input  id="login-email" type="email" name="email" value="">
+        <input  id="login-email" type="email" name="email" value="michael@zenbox.de">
 
         <label for="login-password">Passwort</label>
-        <input  id="login-password" type="password" name="password" value="">
+        <input  id="login-password" type="password" name="password" value="hallo!">
 
         <button type="submit">einloggen</button>
 
       </fieldset>
 
     </form>
+    <!-- /Formular bei Erstaufruf -->
+
+    <!-- Ergebnis bei Login -->
+    <h1>Greetings</h1>
+    <ul>
+      <li>Hallo User ...</li>
+    </ul>
+    <!-- /Ergebnis bei Login -->
 
   </body>
 </html>
