1c193d737132642b1e0e8235ed2e9499b295f789
zenbox <michael@zenbox.de>
2018-02-09 10:31:49 +0100

    09.02.2018

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,85 @@
   // declaration
   let
     loginForm = document.querySelector('#form-login'),
-    onLoginFormSubmit = null;
+    onLoginFormSubmit = null,
+    ajax = null,
+    responseData = {};
 
   // methods
+  ajax = function (url, dataType) {
+    // declaration
+    let request = new XMLHttpRequest(),
+      onReadyStateChange = null,
+      data = {};
+
+    data.email = 'michael@zenbox.de';
+    data.password = 'hallo!';
+
+    // methods
+    onReadyStateChange = function () {
+      switch (request.readyState) {
+      case 0:
+        console.log('no request!');
+        break;
+      case 1:
+        console.log('request opened!');
+        break;
+      case 2:
+        console.log('request sent!');
+        break;
+      case 3:
+        console.log('response head & first data!');
+        console.log(request);
+        if (request.status !== 200) {
+          console.log('no data available!');
+        }
+        break;
+      case 4:
+        console.log('response more data & quit!');
+        if (request.status === 200) {
+          console.log(request.responseText);
+          console.log(request.responseXML);
+
+          if (dataType === 'json') {
+            responseData = JSON.parse(request.responseText);
+            console.table(responseData);
+          }
+        }
+        break;
+      }
+    };
+
+    // control
+    request.addEventListener('readystatechange', onReadyStateChange);
+    request.open('GET', url);
+    request.send(JSON.stringify(data));
+
+
+
+    // var request;
+    // if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    //   request = new XMLHttpRequest();
+    // } else if (window.ActiveXObject) { // IE
+    //   try {
+    //     request = new ActiveXObject('Msxml2.XMLHTTP');
+    //   }
+    //   catch (e) {
+    //     try {
+    //       request = new ActiveXObject('Microsoft.XMLHTTP');
+    //     }
+    //     catch (e) {}
+    //   }
+    // }
+  };
+
   onLoginFormSubmit = function (event) {
+    // declaration
+    let url = event.target.action;
     // stoppt die Browserverarbeitung des Ereignisses
     event.preventDefault();
 
-    console.log('runs');
+    // tue das nächste
+    ajax(url, 'json');
   };
 
   // control, event control
