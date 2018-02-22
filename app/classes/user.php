046da423131956f824a8248355be97f35ffccdd2
zenbox <michael@zenbox.de>
2018-02-22 10:43:52 +0100

    22.02.2018

---
+++ app/classes/user.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * A User Class
+ *
+ * @package Webapplication
+ * @author Michael [michael@zenbox.de]
+ * @since 2018/02/22
+ * @version v1.0.0
+ * @copyright (c) 2018 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+class User 
+{
+  $userName
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-#Code SNIPPETS
+# Code Snippets
 
 '''
 # - - - - - - - - - -
