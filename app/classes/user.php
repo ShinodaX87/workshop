05e3c3717f84ab921c6adcb5cfbfe87ce83ebb96
zenbox <michael@zenbox.de>
2018-02-22 13:10:09 +0100

    22.02.2018

--- app/classes/user.php
+++ app/classes/user.php
@@ -9,7 +9,74 @@
  * @copyright (c) 2018 Michael Reichart, Cologne
  * @license MIT License [https://opensource.org/licenses/MIT]
  */
-class User 
+class User
 {
-  $userName
+    // properties
+    private $property  = null;
+    private $username  = null;
+    private $email     = null;
+    private $password  = null;
+    private $lastLogin = null;
+    private $surname   = null;
+    private $lastname  = null;
+    private $location  = null;
+    private $role      = null;
+
+    // - - - - - - - - - -
+    // magic methods
+    // - - - - - - - - - -
+    public function __construct($s = null)
+    {
+        if (!empty($s)) {
+            $this->surname = $s;
+        }
+    }
+
+    public function __destruct()
+    {
+    }
+
+    // - - - - - - - - - -
+    // own methods
+    // - - - - - - - - - -
+    public function doSomething()
+    {
+        return true;
+    }
+
+    // - - - - - - - - - -
+    // getter and setter
+    // - - - - - - - - - -
+    /**
+     * Getter For Property
+     * @return string returns the user surname
+     */
+    public function getProperty ()
+    {
+        return $this->property;
+    }
+    /**
+     * Setter For Property
+     * @param string $argument the users surname
+     */
+    public function setProperty ($argument = null)
+    {
+      $this->property = $argument;
+    }
+
+
+
+
+    public function getSurname () {
+      if ($this->role === 'default') return $this->surname;
+      return false;
+    }
+
+    public function setSurname ($argument = null)
+    {
+      if (!empty($argument) && is_string($argument)) {
+        $this->surname = trim($argument);
+    }
+
+
 }
--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-// C:/MAMP/htdocs/workshop
 <?php
+// C:/MAMP/htdocs/workshop
+/**
+ * The App Control
+ *
+ * @package Webapplication
+ * @author Michael [michael@zenbox.de]
+ * @since 2018/02/22
+ * @version v1.0.0
+ * @copyright (c) 2018 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+// @todo zum Laufen bringen!!
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
 
-include_once(); // bei Fehlern macht er weiter!
-// default:
-require_once('app/classes/user.php'); // führt bei Fehlern zum Abruch
+// require is default inclusion for classes
+require_once('app/classes/User.php');
+
+// instantiating an object from a class
+$user = new User('Michael');
+
+// late setting a property's value
+$user->setSurname('Karl');
+
+// getting a property's value
+echo $user->getSurname();
