81d669ce1ec2334936c2ab71545b03be0492a8fa
zenbox <michael@zenbox.de>
2018-07-12 13:39:21 +0200

    12.07.2018

--- assets/js/axes-bottom.js
+++ assets/js/axes-bottom.js
@@ -10,11 +10,12 @@
   // declaration
   let
     dataset = [
-      [5, 20],
-      [1300, 90],
-      [250, 50],
-      [100, 33],
-      [330, 95]
+      // x, y, r
+      [5, 20, 100],
+      [1300, 90, 1000],
+      [250, 50, 50],
+      [100, 33, 320],
+      [330, 95, 710]
     ],
 
     canvas = undefined,
@@ -22,6 +23,7 @@
     scale = undefined,
     xScale = undefined,
     yScale = undefined,
+    rScale = undefined,
     xAxis = undefined,
     yAxis = undefined,
     w = undefined,
@@ -33,6 +35,7 @@
     setCanvas = undefined,
     setXScale = undefined,
     setYScale = undefined,
+    setRScale = undefined,
     setXAxis = undefined,
     setYAxis = undefined;
 
@@ -77,6 +80,20 @@
         start: canvas.height - canvas.padding.bottom,
         end: canvas.padding.top
       }
+    },
+    radius: {
+      domain: {
+        from: d3.min(dataset, function (d, i) {
+          return d[2] + 5;
+        }),
+        to: d3.max(dataset, function (d, i) {
+          return d[2] + 5;
+        })
+      },
+      range: {
+        start: 0,
+        end: 10
+      },
     }
   };
 
@@ -100,6 +117,10 @@
     .domain([scale.yAxis.domain.from, scale.yAxis.domain.to])
     .range([scale.yAxis.range.start, scale.yAxis.range.end]);
 
+  rScale = d3.scaleLinear()
+    .domain([scale.radius.domain.from, scale.radius.domain.to])
+    .range([scale.radius.range.start, scale.radius.range.end]);
+
   xAxis = d3.axisBottom()
     .scale(xScale)
     .ticks(5);
@@ -115,6 +136,27 @@
   svg.append('g')
     .attr('transform', 'translate(' + (canvas.padding.left) + ', 0)')
     .call(yAxis);
+
+  /**
+        [1300, 90], -> d
+        [250, 50],
+        [100, 33],
+        [330, 95]
+  */
+  svg.selectAll()
+    .data(dataset) // [[1],[2],[3],[4],[5]]
+    .enter()
+    .append('circle')
+    .attr('cx', function (d, i) {
+      // i=0: d -> [1300, 90]
+      return xScale(d[0]);
+    })
+    .attr('cy', function (d, i) {
+      return yScale(d[1]);
+    })
+    .attr('r', function (d, i) {
+      return rScale(d[2]);
+    });
 
   // - - - - - - - - - -
 }())
